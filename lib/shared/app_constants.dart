4a0f60fb7e8462c8284b94587e33489ff0c6bd22
Seif Salman <seifxofficial@gmail.com>
2024-03-03 13:01:16 +0200

    Initial commit

---
+++ lib/shared/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  static const String baseURL = 'https://api.themoviedb.org/3';
+  static const String imageURLW500 = 'https://image.tmdb.org/t/p/w500';
+  static const String imageURLORIGINAL = 'https://image.tmdb.org/t/p/original';
+  static const String apiKey = '384b2adc17678b68e6fd182930316aa6';
+  static const String accessTokenAuth = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODRiMmFkYzE3Njc4YjY4ZTZmZDE4MjkzMDMxNmFhNiIsInN1YiI6IjY1ZTQ0ZmM1Yzk5ODI2MDE3YjYzNjY0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jMtxJVe-FPkfN-vxF9PN_oGUsdFm9NX3Y1OMjFu2nD4';
+}
